9ff3219d709982c2ef79d0f251c90351d7d818e2
Devik100 <74122137+Devik100@users.noreply.github.com>
2022-03-12 12:11:25 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 function adduser() {
-    player1name=document.getElementById("player1_name").value
-    player2name=document.getElementById("player2_name").value
-    localStorage.setItem("player1", player1name)
-    localStorage.setItem("player2", player2name)
-    window.location="iindex.html"
-}
-player1=localStorage.getItem("player1")
-player2=localStorage.getItem("player2")
+    p1=document.getElementById("player1_name").value
+    p2=document.getElementById("player2_name").value
+    localStorage.setItem("player1", p1)
+    localStorage.setItem("player2", p2)
+    window.location="iindex.html"}
+p1=localStorage.getItem("player1")
+p2=localStorage.getItem("player2")
 score1=0
 score2=0
-document.getElementById("name1").innerHTML=player1 + " : "
-document.getElementById("name2").innerHTML=player2 + " : "
+document.getElementById("name1").innerHTML=p1 + " : "
+document.getElementById("name2").innerHTML=p2 + " : "
 document.getElementById("player1_score").innerHTML=score1
 document.getElementById("player2_score").innerHTML=score2
-document.getElementById("player_question").innerHTML="Question Turn : " + player1
-document.getElementById("player_answer").innerHTML="Answer Turn : " + player2
+document.getElementById("player_question").innerHTML="Question Turn : " + p1
+document.getElementById("player_answer").innerHTML="Answer Turn : " + p2
 function send(){
     Num1=document.getElementById("Number1").value
     Num2=document.getElementById("Number2").value
@@ -26,10 +25,33 @@ function send(){
     row=question_prompt_code+text_input_code+button_check_code
     document.getElementById("output").innerHTML=row
     document.getElementById("Number1").value=""
-    document.getElementById("Number2").value=""
-}
-/*function check() {
-    if (actual_answer==document.getElementById("input_check_box")) {
-        //I don't know what goes here, probably defining ansTurn and quesTurn or something.
-    }
-}*/
+    document.getElementById("Number2").value=""}
+qT=p2
+aT=p1
+function check() {
+    console.log(document.getElementById("input_check_box").value)
+    console.log(actual_answer)
+    console.log(actual_answer.toString() == "NaN")
+    console.log(actual_answer==parseInt(document.getElementById("input_check_box").value))
+    if (actual_answer.toString() == "NaN") {
+        document.getElementById("nothing").innerHTML="no really"}
+    else {
+        if (actual_answer==parseInt(document.getElementById("input_check_box").value)) {
+            if (aT == "p1") {
+                score1 += 1
+                document.getElementById("player1_score").innerHTML = score1}
+            else {
+                score2 += 1
+                document.getElementById("player2_score").innerHTML = score2}}
+        if (aT == "p1") {
+            aT = "p2"
+            qT = "p1"
+            document.getElementById("player_question").innerHTML="Question Turn : " + p2
+            document.getElementById("player_answer").innerHTML="Answer Turn : " + p1}
+        else {
+            aT = "p1"
+            qT = "p2"
+            document.getElementById("player_question").innerHTML="Question Turn : " + p1
+            document.getElementById("player_answer").innerHTML="Answer Turn : " + p2}
+        document.getElementById("nothing").innerHTML="Solve"}
+    document.getElementById("output").innerHTML=""}
